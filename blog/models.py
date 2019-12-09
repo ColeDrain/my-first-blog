2c08c81d45162d0555938a94ddabaebefe54dd9e
ColeDrain <cole.drain@yahoo.com>
2019-12-09 02:42:48 +0100

    modified blog/models.py to show no of comments.

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,9 @@ class Post(models.Model):
 		self.published_date = timezone.now()
 		self.save()
 
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
 	def __str__(self):
 		return self.title
 
@@ -27,9 +30,6 @@ class Comment(models.Model):
     def approve(self):
         self.approved_comment = True
         self.save()
-
-    def approved_comments(self):
-    	return self.comments.filter(approved_comment=True)
 
     def __str__(self):
         return self.text
